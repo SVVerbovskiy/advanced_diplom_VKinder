e5748eb2e80e761da10e7a3f7249e8db1fc53b1c
administrator <P@ssw0rd>
2022-10-30 12:57:42 +0300

    renamed file vk_api.py to vk_search.py

---
+++ vk/vk_search.py
@@ -0,0 +1,109 @@
+import requests
+from config import vk_token
+
+
+class VkClient:
+    url = "https://api.vk.com/method/"
+
+    def __init__(self, token: str):
+        self.token = token
+
+    def search_users(self, offset: str, count: str, hometown: str, sex: str, age: str):
+        url = self.url + "users.search"
+        params = {
+            "access_token": self.token,
+            "v": "5.131",
+            "sort": "0",
+            "offset": offset,
+            "count": count,
+            "hometown": hometown,
+            "sex": sex,
+            "age_from": age,
+            "age_to": age,
+            "has_photo": "1",
+        }
+
+        response = requests.get(url=url, params=params)
+        if response.ok:
+            if "error" not in response.json():
+                return [_ for _ in response.json()["response"]["items"]]
+        else:
+            return None
+
+    def get_profile_photos(self, owner_id: str):
+        url = self.url + "photos.get"
+        params = {
+            "access_token": self.token,
+            "v": "5.131",
+            "owner_id": owner_id,
+            "album_id": "profile",
+            "extended": "1",
+            "photo_sizes": "1",
+        }
+
+        response = requests.get(url=url, params=params)
+        if response.ok:
+            if "error" not in response.json():
+                return response.json()["response"]["items"]
+        else:
+            return None
+
+
+def get_three_popular_profile_photos(profile_photos: list):
+    if len(profile_photos) >= 3:
+        likes = []
+        popular_profile_photos = []
+
+        for photo in profile_photos:
+            likes.append(photo["likes"]["count"])
+        likes = list(set(likes))
+        likes.sort(reverse=True)
+        likes = likes[:3]
+
+        for item in likes:
+            for photo in profile_photos:
+                if photo["likes"]["count"] == item:
+                    popular_profile_photos.append(photo)
+
+        return popular_profile_photos[:3]
+
+    else:
+        return None
+
+
+def get_potential_friends(client: VkClient, sex: str, hometown: str, age: str):
+    if int(sex) == 1:
+        desired_sex = "2"
+    else:
+        desired_sex = "1"
+
+    potential_friends = client.search_users(
+        offset="0", count="1000", hometown=hometown, sex=desired_sex, age=age
+    )
+
+    return potential_friends
+
+
+def get_potential_friend_photos(client: VkClient, owner_id: str):
+    photos = client.get_profile_photos(owner_id=owner_id)
+    if photos is None or len(photos) < 3:
+        return None
+
+    popular_photos = get_three_popular_profile_photos(photos)
+
+    return [_["sizes"][-1]["url"] for _ in popular_photos]
+
+
+if __name__ == "__main__":
+    # создаем экземпляр класса vk клиента
+    vk_client = VkClient(vk_token)
+
+    # ищем потенциальных друзей по критериям
+    my_potential_friends = get_potential_friends(
+        client=vk_client, sex="2", hometown="Москва", age="20"
+    )
+    print(my_potential_friends)
+
+    # получаем три популярные фотографии по id пользователя
+    my_potential_friend_photos = get_potential_friend_photos(client=vk_client, owner_id="199424919")
+    print(my_potential_friend_photos)
